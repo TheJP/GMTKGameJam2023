3d8f4c02cf33efd46c05dfbf83f02108fe4f3a49
Robin Renggli <robin.k.renggli@gmail.com>
2023-07-07 22:54:17 +0200

    add player stats

---
+++ GMTKGameJam2023/PlayerStats.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@export var movement_speed = 3
+@export var claw_attack_speed = 1
+@export var bite_attack_speed = 3
+@export var claw_damage = 1
+@export var bite_damage = 1
+@export var claw_range = 1
+@export var bite_range = 1
+@export var scariness = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
