dba6dc8c5a54b4d95d8496a764bbe44d551195b7
velvence <g.wiederkehr@outlook.com>
2023-07-07 21:40:23 +0200

    Added Transition Animation

---
+++ GMTKGameJam2023/Effects/TransitionScreen.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+@onready var animation_player = $AnimationPlayer
+
+func fade_to_black():
+	animation_player.play("fade_to_black")
+	await animation_player.animation_finished
+	
+func fade_to_normal():
+	animation_player.play("fade_to_normal")
+	await animation_player.animation_finished
+
+func transition():
+	animation_player.play("fade_to_black")
+	
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "fade_to_black":
+		$AnimationPlayer.play("fade_to_normal")
+		
