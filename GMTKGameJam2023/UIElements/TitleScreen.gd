18952b8f9d297e810f4b12fce39700b1bcd652b3
velvence <g.wiederkehr@outlook.com>
2023-07-07 21:54:25 +0200

    UI controlled by WASD Keys

--- GMTKGameJam2023/UIElements/TitleScreen.gd
+++ GMTKGameJam2023/UIElements/TitleScreen.gd
@@ -1,5 +1,10 @@
 extends Control
 
+@onready var start_game_button = %"Start Game Button"
+
+func _ready():
+	start_game_button.grab_focus()
+
 func _on_start_game_button_pressed():
 	get_tree().paused = true
 	await TransitionScreen.fade_to_black()
