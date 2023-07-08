c4e752ebacbaba93cf3896eedc23cc00735909fe
TheJP <jp_55555@hotmail.com>
2023-07-08 13:36:54 +0200

    Added bite animation

---
+++ GMTKGameJam2023/Player/attack_bite.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var animation = preload("res://player/bite_animation.tscn")
+
+func _on_attack_damage_target(body: PhysicsBody2D):
+	var attack = animation.instantiate()
+	attack.position = body.position
+	add_child(attack)
