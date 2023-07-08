cf3e730a4de5c8254033b316bc186446146cbbb9
Robin Renggli <robin.k.renggli@gmail.com>
2023-07-08 14:35:37 +0200

    initialize player stats

--- GMTKGameJam2023/PlayerStats.gd
+++ GMTKGameJam2023/PlayerStats.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@export var movement_speed = 3
+@export var movement_speed = 500
 @export var liferegeneration = 1
 @export var claw_attack_speed = 1
 @export var bite_attack_speed = 3
@@ -10,5 +10,11 @@ extends Node
 @export var bite_range = 1
 @export var scariness = 1
 
-
+func initialize_player_stats():
+	$"../GameCanvasLayer/Player".speed = movement_speed
+	$"../GameCanvasLayer/Player/AttackClaws".damage = claw_damage
+	$"../GameCanvasLayer/Player/AttackClaws/Timer".start(claw_attack_speed)
+	$"../GameCanvasLayer/Player/AttackBite".damage = bite_damage
+	$"../GameCanvasLayer/Player/AttackBite/Timer".start(bite_attack_speed )
+	
 
