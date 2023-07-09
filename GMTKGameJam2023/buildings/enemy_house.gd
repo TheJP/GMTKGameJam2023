244b0a5564e031a82dad79f793b21fdc585b4c6c
SilvanRenggli <sili.renggli@gmail.com>
2023-07-09 14:29:17 +0200

    Make building destroyable

---
+++ GMTKGameJam2023/buildings/enemy_house.gd
@@ -0,0 +1,8 @@
+extends StaticBody2D
+
+func _on_entity_health_changed(old_health, new_health):
+	if new_health <= 0:
+		$EnemySpawner/SpawnTimer.stop()
+		$AnimatedSprite2D.visible = false
+		$Sprite2D.visible = true
+		remove_from_group("enemy")
--- GMTKGameJam2023/entity/entity.gd
+++ GMTKGameJam2023/entity/entity.gd
@@ -3,7 +3,7 @@ extends Node
 
 signal health_changed(old_health: int, new_health: int)
 
-@export var health = 100 : set = on_health_changed
+@export var health = 500 : set = on_health_changed
 
 func on_health_changed(new_health):
 	health_changed.emit(health, new_health)
